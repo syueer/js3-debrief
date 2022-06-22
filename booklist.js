477d82773b5063981601605c95dee33607f5d625
syueer <syue531@hotmail.com>
2022-06-22 19:58:49 +1000

    exercise

--- booklist.js
+++ booklist.js
@@ -5,38 +5,42 @@ class BookManager {
   // this is a constructor with a paramater currentId set to 0 by default
   constructor(currentId = 0) {
     // 1. initialize an empty array called books
-
+    this._books = []
     // 2. initialize a currentId set to currentId
+    this._currentId = currentId
   }
 
   // 3. a getter function should return the value of our books array
   get books() {
-
+    return this._books
   }
 
   // function addBook should create an object and push it to our books array
   // it should take in a name, genre and author and by default have an empty string
-  addBook() {
+  addBook(name = '', genre = '', author = '') {
     // 4. Create an object assigned to a variable for the new book using parameters given to addBook
-
+    let newBook = { id: this._currentId, name: name, genre: genre, author: author }
     // a) your object should consist of properties id, name,genre and author with values added when we call this method
     // don't forget that this may require some parameters to be used as values
     // b) id should be unique and increment by 1 every new instance
     // c) if no argument is given, the properties should equal an empty string
-
+    this._currentId++
     // 5. Push the newly created object to our books array
     // hint: note that best practice would be to access our books array using the get method
-
+    this._books.push(newBook)
   }
 
 }
 
+const newManager = new BookManager()
 // 6. outside the class, create an instance of BookManager
-
+console.log(newManager.books)
 // 7. add two books with the following properties to our instance
 // "The Alchemist", "Life", "Paulo Coelho"
 // "Time to Play", "Children's Book", "Bluey"
-
+newManager.addBook("The Alchemist", "Life", "Paulo Coelho")
+newManager.addBook("Time to Play", "Children's Book", "Bluey")
+newManager.addBook()
 // 8. console log our books array to check that the new books were added
-
+console.log(newManager.books)
 
--- exercises.js
+++ exercises.js
@@ -9,6 +9,18 @@
 // filling the blanks with the relevant values - (remember to do the above using the ‘this’ keyword)
 // c) Outside the class, Call the function ‘displayInfo’
 
+const school = {
+  name: "Hogwarts",
+  location: "Unknown",
+  established: "the 10th Century",
+  displayInfo() {
+    console.log(`My school ${this.name} was established in ${this.location} at ${this.established}`)
+  }
+}
+
+console.log(school['name'])
+school.displayInfo()
+
 // Classes
 
 // We will now create a template for making school objects! You may want to refer to Class Syntax: https://www.w3schools.com/js/js_classes.asp ;
@@ -17,3 +29,16 @@
 // b) add a method called displayInfo that console.logs the properties
 // c) Using this class, create a new instance (object) with the values of the object we made above
 // d) call this Instance's (this new object created from the template) displayInfo();
+class CreateSchool {
+  constructor(name, location, established) {
+    this._name = name;
+    this._location = location;
+    this._established = established;
+  }
+  displayInfo() {
+    console.log(`${this._name} ${this._location} ${this._established}`)
+  }
+}
+
+const newSchool = new CreateSchool('Yue', 'abcd school', 2022);
+newSchool.displayInfo()
